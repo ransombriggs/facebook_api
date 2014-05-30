67336ad128ac60cc088e23f6abb6e983e038e9ea
Ransom Briggs <ransom-briggs@uiowa.edu>
2014-05-29 22:12:51 -0500

    Trying to understand how facebook login acl
    * it appears to just check hostname and port

---
+++ app/controllers/Application.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+    public static void rbriggs_test_site() {
+        render();
+    }
+
+    public static void rbriggs_other_test_site() {
+        render();
+    }
+
+}
